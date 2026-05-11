7adbfc6ec9d1d542129f928a1e371e0eb1a40b05
Josue9822 <91173243+Josue9822@users.noreply.github.com>
2026-05-11 21:12:05 +0000

    cambios v6

--- app.py
+++ app.py
@@ -684,21 +684,15 @@ else:
 
             /* Avatar con Inicial */
             .user-avatar {
-                background: #FFFFFF;
-                color: #990000 !important; /* Forzamos el color rojo de la letra */
-                width: 60px;
-                height: 60px;
-                
-                /* Centrado absoluto */
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                
+                background-color: #990000;
+                color: white;
+                width: 50px;
+                height: 50px;
+                line-height: 50px;
                 border-radius: 50%;
-                font-size: 24px;
-                font-weight: 800;
-                margin: 0 auto 12px auto;
-                box-shadow: 0 4px 10px rgba(0,0,0,0.3);
+                font-size: 20px;
+                font-weight: bold;
+                margin: 0 auto 8px auto;
             }
 
             /* Textos del Usuario */
